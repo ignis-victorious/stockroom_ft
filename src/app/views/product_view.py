3c7741bdb69199c33d1ef1d3532553790bde5d3b
Emmanuele Levi <ignisvictorious@gmail.com>
2025-07-25 00:40:30 +0100

    Mostly working still problems with add

--- src/app/views/product_view.py
+++ src/app/views/product_view.py
@@ -1,11 +1,25 @@
 #  ___________________
 #  Import LIBRARIES
+from sqlite3 import Cursor
+
 import flet as ft  # type: ignore
-from flet import AppBar, Column, ElevatedButton, IconButton, Icons, Page, Row, Text, TextField  # type: ignore
+from flet import (  # type: ignore
+    AppBar,
+    Column,
+    Divider,
+    ElevatedButton,
+    IconButton,
+    Icons,
+    ListTile,
+    ListView,
+    Page,
+    Row,
+    Text,
+    TextField,
+)
 
 #  Import FILES
-# from ..models.database import get_connection
-
+from ..models.database import get_connection
 
 #  ___________________
 
@@ -19,6 +33,7 @@ class ProdutsView:
         self.product_price = TextField(label="Product's Price")
         # Quantidade do produto: Product's Quantity
         self.product_quantity = TextField(label="Product's Quantity")
+        self.list_products_view = ListView()
 
     def build(self) -> None:
         self.page.controls.clear()
@@ -42,15 +57,67 @@ class ProdutsView:
                                 text="Product's registration",
                                 on_click=self._register_product,
                             )
-                        ]
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
                     ),
-                ]
+                    Divider(),
+                    #  Produtos cadastrados: Products registered
+                    Text(value="Products registered", size=20, weight=ft.FontWeight.BOLD),
+                    self.list_products_view,
+                ],
+                expand=True,
+                scroll=ft.ScrollMode.AUTO,
             )
         )
+        self.products_list()
         self.page.update()
 
     # Função para cadastrar produto
-    def _register_product(self, e: ft.ControlEvent): ...
+    def _register_product(self, e: ft.ControlEvent) -> None:
+        name = self.product_name.value.strip()
+        try:
+            price = float(self.product_price.value.strip())
+            quantity = int(self.product_quantity.value.strip())
+        except:
+            #  O preço e a quantidade devem ser números!: The price and quantity must be numbers!
+            print("The price and quantity must be numbers!")
+            return
+
+        if not name:
+            print("Preencha o nome do produto.")
+            return
+
+        with get_connection() as conn:
+            cursor: Cursor = conn.cursor()
+            # produtos: products
+            cursor.execute("""INSERT INTO products (name, price, quantity) VALUES (?, ?, ?)""", (name, price, quantity))
+            conn.commit()
+
+            #  Produto cadastrado com sucesso!: Product successfully registered!
+            print("Product successfully registered!")
+
+            self.product_name.value = ""
+            self.product_price.value = ""
+            self.product_quantity.value = ""
+            self.product_list()
+            self.page.update()
+
+    def products_list(self) -> None:
+        self.list_products_view.controls.clear()
+        # self.list_products.controls.clear()
+        with get_connection() as conn:
+            cursor: Cursor = conn.cursor()
+            #  produtos: products
+            cursor.execute("SELECT name, price, quantity FROM products")
+            for name, price, quantity in cursor.fetchall():
+                self.list_products_view.controls.append(
+                    ListTile(
+                        title=Text(value=f"{name}"),
+                        #  Preço: (price: 2f) MZN | Quantidade: [quantity} =
+                        subtitle=Text(value=f"Price: {price: .2f} MZN | Quantity: {quantity}"),
+                    )
+                )
+                self.page.update()
 
     def _go_back(self) -> None:
         from app.views.home_view import HomeView
