1671a6b1442caf3cdc18af5e8aab513bf5f9d5da
Emmanuele Levi <ignisvictorious@gmail.com>
2025-07-24 22:09:17 +0100

    Solved a few type hinting issues and errors. Some page.add do not display

---
+++ src/app/views/product_view.py
@@ -0,0 +1,58 @@
+#  ___________________
+#  Import LIBRARIES
+import flet as ft
+from flet import AppBar, IconButton, Icons, Page, Text, TextField  # type: ignore
+
+#  Import FILES
+# from ..models.database import get_connection
+
+
+#  ___________________
+
+
+class ProdutsView:
+    def __init__(self, page: Page) -> None:
+        self.page = page
+        # Nome do produto: Product's name
+        self.product_name = TextField(label="Product's name")
+        # Preço do produto: Product's Price
+        self.product_price = TextField(label="Product's Price")
+        # Quantidade do produto: Product's Quantity
+        self.product_quantity = TextField(label="Product's Quantity")
+
+    def build(self) -> None:
+        self.page.controls.clear()
+
+        self.page.appbar = AppBar(
+            #  Cadastro de Produtos: Product registration
+            title=Text(value="Product registration", size=24, weight=ft.FontWeight.BOLD),
+            leading=IconButton(icon=Icons.ARROW_BACK, on_click=lambda e: self._go_back()),
+        )
+
+        # self.page.add(
+        #     Column(
+        #         controls=[
+        #             self.product_name,
+        #             self.product_price,
+        #             self.product_quantity,
+        #             Row(
+        #                 controls=[
+        #                     ElevatedButton(
+        #                         #  Cadastar produto: Product's registration
+        #                         text="Product's registration",
+        #                         on_click=self._register_product,
+        #                     )
+        #                 ]
+        #             ),
+        #         ]
+        #     )
+        # )
+
+    # Função para cadastrar produto
+    def _register_product(self, e: ft.ControlEvent): ...
+
+    def _go_back(self) -> None:
+        from app.views.home_view import HomeView
+
+        home = HomeView(page=self.page)
+        home.build()
